48fbe17d12265240130d9e071f4327991a57ed33
Kali Live user <nolan.ameline@gmail.com>
2026-03-03 16:40:26 +0000

    Nouveaux main

--- TP1/src/Exo12.c
+++ TP1/src/Exo12.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    char c;
+int nb_Voyelles(char *texte){
     int nbVoyelles = 0;
 
-    printf("Entrez un texte (terminez par #) :\n");
+    char c;
 
-    while (1) {
-        scanf("%c", &c);
-
-        if (c == '#') {
-            break;
-        }
-
+    for(int i=0; i++; *(texte+i) != '#'){
+        c = *(texte + i);
+        
+    
         if (c == 'a' || c == 'e' || c == 'i' || 
-            c == 'o' || c == 'u' || c == 'y' ||
-            c == 'A' || c == 'E' || c == 'I' ||
-            c == 'O' || c == 'U' || c == 'Y') {
-            
-            nbVoyelles++;
-        }
+        c == 'o' || c == 'u' || c == 'y' ||
+        c == 'A' || c == 'E' || c == 'I' ||
+        c == 'O' || c == 'U' || c == 'Y') {
+        
+        nbVoyelles++;
     }
-
+        
+    }
+    
     printf("Nombre de voyelles : %d\n", nbVoyelles);
+    return nb_Voyelles;
 
-    return 0;
+}
+int main() {
+    char *texte;
+
+    printf("Entrez le texte : ");
+    scanf("%s", &texte);
+
+    nb_Voyelles(texte);
+
 }
