475196154fd987fd0c96188b68f9ac6f4ea1c722
petit_gravier <benjaminborne4@gmail.com>
2026-03-03 17:14:51 +0100

    TP1 sans test

---
+++ TP1/src/division.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    printf("X = %d\n", 10/3);
+    // Calcul d'abord en int puis cast le résultat en float d'où le 3.0000000...
+    printf("X = %f\n", (float)(10/3));
+    printf("X = %f\n", 10.0/3);
+    printf("X = %f\n", 10.0/3.0);
+    printf("X = %f\n", (float)10/3);
+    printf("X = %f\n", ((float)10)/3);
+    printf("X = %.1f\n", ((float)10)/3);
+    printf("X = %.5f\n", ((float)10)/3);
+
+    return 0;
+}
---
+++ TP1/src/somme.c
@@ -0,0 +1,55 @@
+#include "stdio.h"
+
+int IncrementLoop(int n) {
+    
+    int sum = 0;
+    for(int i=1; i<=n; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int DecrementLoop(int n) {
+    int sum = 0;
+
+    for (int i = n; i>=1; i--) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int WhileSumIncrement(int n) {
+    int sum = 0;
+    int i = 1;
+
+    while (i<=n) {
+        sum += i;
+        i++;
+    }
+
+    return sum;
+}
+
+int WhileSumDecrement(int n) {
+    int sum = 0;
+    int i = n;
+
+    while (i>0) {
+        sum += i;
+        i--;
+    }
+
+    return sum;
+}
+
+int main (void) {
+    printf("Somme for incrémentation: %d\n", IncrementLoop(10));
+    printf("Somme for décrémentation: %d\n", DecrementLoop(10));
+    printf("Somme While Incrementation: %d\n", WhileSumIncrement(10));
+    printf("Somme While Decrementation: %d\n", WhileSumDecrement(10));
+
+    return 0;
+}
+
