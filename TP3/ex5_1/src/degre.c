d7c6d3cd9823547430becca4ddb27c442e394b86
petit_gravier <benjaminborne4@gmail.com>
2026-03-29 19:12:05 +0200

    Sauvegarde avant merge

--- TP3/ex5_1/src/degre.c
+++ TP3/ex5_1/src/degre.c
@@ -1,12 +1,37 @@
-#include <stdio.h>      
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include "degre.h"
 
 
 int main(void){
+    char *endptr;
     double celsius, kelvin, fahrenheit;
-
+    char input[64];
     puts("Entrez une température en Celsius : ");
-    scanf("%lf", &celsius);
+
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+        fprintf(stderr, "Erreur de lecture\n");
+        return 1;
+    }
+    input[strcspn(input, "\n")] = '\0';
+
+    errno = 0;
+    celsius = strtod(input, &endptr);
+
+    if (endptr == input) {
+        fprintf(stderr, "Entrée invalide : aucun chiffre trouvé\n");
+        return -1;
+    }
+    if (*endptr != '\0') {
+        fprintf(stderr, "Caractères inattendus après le nombre : '%s'\n", endptr);
+        return -1;
+    }
+    if (errno == ERANGE) {
+        fprintf(stderr, "Valeur hors plage\n");
+        return -1;
+    }
 
     kelvin = CelsiusAKelvin(celsius);
     fahrenheit = CelsiusAFahrenheit(celsius);
